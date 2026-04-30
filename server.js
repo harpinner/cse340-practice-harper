5c8513d1484d7b2a11adc12682240a8a454b0f4e
harpinner <harpinner@users.noreply.github.com>
2026-04-29 20:52:26 -0400

    it is arisen again

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path';
 // Import express using ESM syntax
 import express from 'express';
 
+
+
 // Create an instance of an Express application
 const app = express();
 
@@ -13,24 +15,67 @@ const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Global template variables middleware
+ * 
+ * Makes common variables available to all EJS templates without having to pass
+ * them individually from each route handler
+ */
+app.use((req, res, next) => {
+    // Make NODE_ENV available to all templates
+    res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
+
+    // Continue to the next middleware or route handler
+    next();
+});
+
+
+
+
 
 /**
  * Routes
  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+      const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+      const title = 'About Me';
+    res.render('about', { title });
 });
 
 app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+      const title = 'Our Products';
+    res.render('products', { title });
 });
 // Define the port number the server will listen on
+
+
+// When in development mode, start a WebSocket server for live reloading
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
+
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort });
+
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server is running on port ${wsPort}`);
+        });
+
+        wsServer.on('error', (error) => {
+            console.error('WebSocket server error:', error);
+        });
+    } catch (error) {
+        console.error('Failed to start WebSocket server:', error);
+    }
+}
 
 
 // Start the server and listen on the specified port
